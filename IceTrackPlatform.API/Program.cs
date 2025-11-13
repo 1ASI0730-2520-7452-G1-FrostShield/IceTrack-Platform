a68b34415f006e53490bf2990af3ab93c023796a
Pedro Nanfuñay <pedro18nan@gmail.com>
2025-11-13 16:05:03 -0500

    feat: update app settings for development.

--- IceTrackPlatform.API/Program.cs
+++ IceTrackPlatform.API/Program.cs
@@ -1,18 +1,85 @@
+using System.Text.Json.Serialization;
+using IceTrackPlatform.API.Reporting.Application.Internal.CommandServices;
+using IceTrackPlatform.API.Reporting.Application.Internal.QueryServices;
+using IceTrackPlatform.API.Reporting.Domain.Repositories;
+using IceTrackPlatform.API.Reporting.Domain.Services;
+using IceTrackPlatform.API.Reporting.Infrastructure.Persistence.EFC.Repositories;
+using IceTrackPlatform.API.Shared.Domain.Repositories;
+using IceTrackPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using IceTrackPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// Configure Lower Case URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+// Add Database Connection
+
+// Configuration Database Context and Logging Levels
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        if (connectionString is null)
+            throw new Exception("Connection string is null");
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    });
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+        var connectionStringTemplate = configuration.GetConnectionString("DefaultConnection");
+        if (connectionStringTemplate is null)
+            throw new Exception("Connection string is null");
+        var connectionString = Environment.ExpandEnvironmentVariables(connectionStringTemplate);
+        if (connectionString is null)
+            throw new Exception("Connection string is null after expanding environment variables");
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error)
+            .EnableDetailedErrors();
+    });
+
+// Configure Dependency Injection for Application Services
+
+// Shared Bounded Context Injections 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// News Bounded Context Injections
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddScoped<IReportQueryServices, ReportQueryService>();
+builder.Services.AddScoped<IReportCommandService, ReportCommandService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
