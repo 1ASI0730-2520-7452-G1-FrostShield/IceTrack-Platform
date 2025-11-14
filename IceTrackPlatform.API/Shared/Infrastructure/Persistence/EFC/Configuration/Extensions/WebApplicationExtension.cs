7064133b43b2e408764789b411278a40511477c5
Kenyi15upc <u202220138@upc.edu.pe>
2025-11-13 23:51:26 -0500

    feat(iam): rename method to UseDatabaseCreationAssurance for clarity and improve scope handling

--- IceTrackPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/WebApplicationExtension.cs
+++ IceTrackPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/WebApplicationExtension.cs
@@ -4,9 +4,10 @@ namespace IceTrackPlatform.API.Shared.Infrastructure.Persistence.EFC.Configurati
 
 public static class WebApplicationExtensions
 {
-    public static void EnsureDatabaseCreated(this WebApplication app)
+    public static void UseDatabaseCreationAssurance(this WebApplication app)
     {
-        // Verify if the database exists and create it if it doesn't
-        app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>().Database.EnsureCreated();
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated();
     }
 }
