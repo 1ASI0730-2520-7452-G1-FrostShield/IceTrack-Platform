e6fedbf171dc3fd4eb8a0e99573a411fb5a37ee4
Kenyi15upc <u202220138@upc.edu.pe>
2025-11-13 22:16:27 -0500

    feat(iam): implement IAM context facade for user operations

---
+++ IceTrackPlatform.API/IAM/Interfaces/ACL/IIamContextFacade.cs
@@ -0,0 +1,31 @@
+﻿using IceTrackPlatform.API.IAM.Domain.Model.ValueObjects;
+
+namespace IceTrackPlatform.API.IAM.Interfaces.ACL;
+
+/// <summary>
+/// Interface for the IAM context facade.
+/// </summary>
+public interface IIamContextFacade
+{
+    /// <summary>
+    /// Creates a new user.
+    /// </summary>
+    /// <param name="username">The username.</param>
+    /// <param name="password">The password.</param>
+    /// <param name="role">The user role.</param> 
+    /// <returns>The user ID.</returns>
+    Task<int> CreateUser(string username, string password, Roles role);
+    /// <summary>
+    /// Fetches the user ID by username.
+    /// </summary>
+    /// <param name="username">The username.</param>
+    /// <returns>The user ID.</returns>
+    Task<int> FetchUserIdByUsername(string username);
+    /// <summary>
+    /// Fetches the username by user ID.
+    /// </summary>
+    /// <param name="userId">The user ID.</param>
+    /// <returns>The username.</returns>
+    Task<string> FetchUsernameByUserId(int userId);
+}
+
