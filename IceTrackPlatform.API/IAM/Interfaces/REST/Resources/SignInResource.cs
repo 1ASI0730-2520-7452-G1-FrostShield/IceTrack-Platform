6a691ff8858d5efdbd11e0f8fc669f1aa33afec7
Kenyi15upc <u202220138@upc.edu.pe>
2025-11-13 22:36:58 -0500

    feat(iam): add resource models for user authentication and management

---
+++ IceTrackPlatform.API/IAM/Interfaces/REST/Resources/SignInResource.cs
@@ -0,0 +1,8 @@
+﻿namespace IceTrackPlatform.API.IAM.Interfaces.REST.Resources;
+
+/// <summary>
+/// Resource for user sign-in requests.
+/// </summary>
+/// <param name="Username">The username for sign-in.</param>
+/// <param name="Password">The password for sign-in.</param>
+public record SignInResource(string Username, string Password);
---
+++ IceTrackPlatform.API/IAM/Interfaces/REST/Resources/SignUpResource.cs
@@ -0,0 +1,11 @@
+﻿using IceTrackPlatform.API.IAM.Domain.Model.ValueObjects;
+
+namespace IceTrackPlatform.API.IAM.Interfaces.REST.Resources;
+
+/// <summary>
+/// Resource for user sign-up requests.
+/// </summary>
+/// <param name="Username">The username for sign-up.</param>
+/// <param name="Password">The password for sign-up.</param>
+/// <param name="Role">The role for sign-up.</param> 
+public record SignUpResource(string Username, string Password, Roles Role); 
