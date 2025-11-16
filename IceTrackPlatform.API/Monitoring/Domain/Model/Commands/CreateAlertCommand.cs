c1c1e8023b771317b167f9b24f62083955e9404e
Alejandro <u202321264@upc.edu.pe>
2025-11-16 18:47:02 -0500

    Update CreateAlertCommand.cs

--- IceTrackPlatform.API/Monitoring/Domain/Model/Commands/CreateAlertCommand.cs
+++ IceTrackPlatform.API/Monitoring/Domain/Model/Commands/CreateAlertCommand.cs
@@ -1,4 +1,4 @@
-﻿using IceTrackPlatform.API.Monitoring.Domain.Model.ValueObjects;
+using IceTrackPlatform.API.Monitoring.Domain.Model.ValueObjects;
 
 namespace IceTrackPlatform.API.Monitoring.Domain.Model.Commands;
 
@@ -10,9 +10,7 @@ namespace IceTrackPlatform.API.Monitoring.Domain.Model.Commands;
 /// <param name="SiteId">Identifier of the site associated with the alert.</param>
 /// <param name="Type">The type or category of the alert.</param>
 /// <param name="Severity">The severity level of the alert.</param>
-/// <param name="Title">Short descriptive title of the alert.</param>
 /// <param name="Description">Detailed explanation of the alert.</param>
-/// <param name="Date">Date and time when the alert occurred.</param>
 /// <param name="Status">Current status of the alert.</param>
 public record CreateAlertCommand(
     int TenantId,
@@ -21,6 +19,5 @@ public record CreateAlertCommand(
     string Type,
     EAlertSeverity Severity,
     string Description,
-    DateTime Date,
     EAlertStatus Status
 );
