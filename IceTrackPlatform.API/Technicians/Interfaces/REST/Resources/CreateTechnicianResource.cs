7b2d7259d994a155703222d0e56fa66ce92b9b93
Kenyi15upc <u202220138@upc.edu.pe>
2025-12-01 17:51:13 -0500

    feat(technicians): add technician resource and transform

---
+++ IceTrackPlatform.API/Technicians/Interfaces/REST/Resources/CreateTechnicianResource.cs
@@ -0,0 +1,8 @@
+﻿namespace IceTrackPlatform.API.Technicians.Interfaces.REST.Resources;
+
+
+public record CreateTechnicianResource(
+    string Name, 
+    string Specialty, 
+    string Phone, 
+    int ProviderId);
---
+++ IceTrackPlatform.API/Technicians/Interfaces/REST/Resources/TechnicianResource.cs
@@ -0,0 +1,8 @@
+﻿namespace IceTrackPlatform.API.Technicians.Interfaces.REST.Resources;
+
+public record TechnicianResource(
+    int Id,
+    string Name,
+    string Specialty,
+    string Phone,
+    int ProviderId);
