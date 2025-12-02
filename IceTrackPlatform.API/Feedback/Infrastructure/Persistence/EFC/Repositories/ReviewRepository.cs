1664bd7945d91a6160d24c4e2f0d1cedbdfab931
Kenyi15upc <u202220138@upc.edu.pe>
2025-12-02 13:14:13 -0500

    feat(feedback):add Reviews repository, command and query services

---
+++ IceTrackPlatform.API/Feedback/Infrastructure/Persistence/EFC/Repositories/ReviewRepository.cs
@@ -0,0 +1,20 @@
+﻿using IceTrackPlatform.API.Feedback.Domain.Model.Aggregates;
+using IceTrackPlatform.API.Feedback.Domain.Repositories;
+using IceTrackPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using IceTrackPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceTrackPlatform.API.Feedback.Infrastructure.Persistence.EFC.Repositories;
+
+public class ReviewRepository(AppDbContext context) : BaseRepository<Review>(context), IReviewRepository
+{
+    public async Task<IEnumerable<Review>> FindByServiceRequestIdAsync(int serviceRequestId)
+    {
+        return await Context.Set<Review>().Where(r => r.ServiceRequestId == serviceRequestId).ToListAsync();
+    }
+
+    public new async Task<IEnumerable<Review>> ListAsync()
+    {
+        return await Context.Set<Review>().ToListAsync();
+    }
+}
