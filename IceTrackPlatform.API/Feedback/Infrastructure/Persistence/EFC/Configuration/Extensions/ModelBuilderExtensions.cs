50410cc39624a48a8de647e39d2491260361038c
Kenyi15upc <u202220138@upc.edu.pe>
2025-12-02 13:18:18 -0500

    feat(feedbacks): add ModelBuilderExtensions

---
+++ IceTrackPlatform.API/Feedback/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using IceTrackPlatform.API.Feedback.Domain.Model.Aggregates;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceTrackPlatform.API.Feedback.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ApplyFeedbackConfiguration(this ModelBuilder builder)
+    {
+        // Review Aggregate
+        builder.Entity<Review>().HasKey(r => r.Id);
+        builder.Entity<Review>().Property(r => r.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Review>().Property(r => r.ServiceRequestId).IsRequired();
+        builder.Entity<Review>().Property(r => r.OwnerId).IsRequired();
+        builder.Entity<Review>().Property(r => r.TechnicianId).IsRequired();
+        builder.Entity<Review>().Property(r => r.Comment).IsRequired(false);
+
+        builder.Entity<Review>()
+            .OwnsOne(r => r.Rating, rr =>
+            {
+                rr.Property(p => p.Value).HasColumnName("Rating");
+            });
+    }
+}
