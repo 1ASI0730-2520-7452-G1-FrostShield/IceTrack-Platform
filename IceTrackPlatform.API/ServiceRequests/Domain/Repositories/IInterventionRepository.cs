2780832b19756244c813254c258b0f47d3ad12d8
Kenyi15upc <u202220138@upc.edu.pe>
2025-12-02 13:29:39 -0500

    feat(service-requests): add intervention repositories

---
+++ IceTrackPlatform.API/ServiceRequests/Domain/Repositories/IInterventionRepository.cs
@@ -0,0 +1,10 @@
+﻿using IceTrackPlatform.API.ServiceRequests.Domain.Model.Aggregates;
+using IceTrackPlatform.API.Shared.Domain.Repositories;
+
+namespace IceTrackPlatform.API.ServiceRequests.Domain.Repositories;
+
+public interface IInterventionRepository : IBaseRepository<Intervention>
+{
+    Task<IEnumerable<Intervention>> FindByServiceRequestIdAsync(int serviceRequestId);
+    new Task<IEnumerable<Intervention>> ListAsync();
+}
