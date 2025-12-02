d8bf84107dc38c952dc0dcf3e433cf6bb3674289
Kenyi15upc <u202220138@upc.edu.pe>
2025-12-02 13:12:18 -0500

    feat(feedbacks): add reviews Domain files

---
+++ IceTrackPlatform.API/Feedback/Domain/Services/IReviewCommandService.cs
@@ -0,0 +1,9 @@
+﻿using IceTrackPlatform.API.Feedback.Domain.Model.Aggregates;
+using IceTrackPlatform.API.Feedback.Domain.Model.Commands;
+
+namespace IceTrackPlatform.API.Feedback.Domain.Services;
+
+public interface IReviewCommandService
+{
+    Task<Review?> Handle(CreateReviewCommand command);
+}
---
+++ IceTrackPlatform.API/Feedback/Domain/Services/IReviewQueryService.cs
@@ -0,0 +1,11 @@
+﻿using IceTrackPlatform.API.Feedback.Domain.Model.Aggregates;
+using IceTrackPlatform.API.Feedback.Domain.Model.Queries;
+
+namespace IceTrackPlatform.API.Feedback.Domain.Services;
+
+public interface IReviewQueryService
+{
+    Task<Review?> Handle(GetReviewByIdQuery query);
+    Task<IEnumerable<Review>> Handle(GetReviewsByServiceRequestIdQuery query);
+    Task<IEnumerable<Review>> Handle(GetAllReviewsQuery query);
+}
